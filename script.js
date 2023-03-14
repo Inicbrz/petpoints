4726659d279af41e6b3f4c248e0f3953e5aaa125
Nicholas Gabriel <109010700+Inicbrz@users.noreply.github.com>
2023-03-14 14:01:39 -0300

    Atualização Numero 1
    Lançamento do site

---
+++ script.js
@@ -0,0 +1,49 @@
+const botaodenavegacao = document.querySelector('.botaodenavegacao');
+const paineldenavegacao = document.querySelector('.paineldenavegacao');
+
+botaodenavegacao.addEventListener('click', ()=> {
+    paineldenavegacao.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+const m5mm1 = document.querySelector('.mais5motivosmostrar1');
+const m5me1 = document.querySelector('.mais5motivosesconder1');
+
+m5mm1.addEventListener('click', ()=> {
+    m5me1.classList.toggle('active');
+});
+
+const m5mm2 = document.querySelector('.mais5motivosmostrar2');
+const m5me2 = document.querySelector('.mais5motivosesconder2');
+
+m5mm2.addEventListener('click', ()=> {
+    m5me2.classList.toggle('active');
+});
+
+const m5mm3 = document.querySelector('.mais5motivosmostrar3');
+const m5me3 = document.querySelector('.mais5motivosesconder3');
+
+m5mm3.addEventListener('click', ()=> {
+    m5me3.classList.toggle('active');
+});
+
+const m5mm4 = document.querySelector('.mais5motivosmostrar4');
+const m5me4 = document.querySelector('.mais5motivosesconder4');
+
+m5mm4.addEventListener('click', ()=> {
+    m5me4.classList.toggle('active');
+});
+
+const m5mm5 = document.querySelector('.mais5motivosmostrar5');
+const m5me5 = document.querySelector('.mais5motivosesconder5');
+
+m5mm5.addEventListener('click', ()=> {
+    m5me5.classList.toggle('active');
+});
+
